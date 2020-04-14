87070d2e2e3422250683ec4dd6cb41c7676f4a4d
Min Gee Suh <suh@student.unimelb.edu.au>
2020-04-14 20:32:21 +0900

    Custom-Build
    Added Alignment
    AutoSave

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -29,6 +29,8 @@ import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefrom
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
 import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+import Autosave from '@ckeditor/ckeditor5-autosave/src/autosave';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -56,7 +58,10 @@ ClassicEditor.builtinPlugins = [
 	PasteFromOffice,
 	Table,
 	TableToolbar,
-	TextTransformation
+	TextTransformation,
+	Autosave,
+	Alignment
+
 ];
 
 // Editor configuration.
@@ -67,15 +72,14 @@ ClassicEditor.defaultConfig = {
 			'|',
 			'bold',
 			'italic',
+			'blockQuote',
 			'link',
 			'bulletedList',
 			'numberedList',
 			'|',
-			'indent',
-			'outdent',
+			'alignment',
 			'|',
 			'imageUpload',
-			'blockQuote',
 			'insertTable',
 			'mediaEmbed',
 			'undo',
